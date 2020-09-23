e6800011d7596a65aadaf3738205e36784ca4f1d
wojtek <wojciech.bilicki@gmail.com>
2020-09-23 20:40:06 +0200

    Resolve #1. Create basic register page

---
+++ src/components/common/InputField.tsx
@@ -0,0 +1,24 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from '@chakra-ui/core';
+import { useField } from 'formik';
+import React, { InputHTMLAttributes } from 'react';
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({label, size, ...props}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} placeholder={props.placeholder} />
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  );
+};
---
+++ src/generated/graphql.tsx
@@ -0,0 +1,225 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  catalogs: Array<Catalog>;
+  catalog: Catalog;
+  getEntries: Array<Entry>;
+  me?: Maybe<User>;
+  hello: Scalars['String'];
+};
+
+
+export type QueryCatalogArgs = {
+  id: Scalars['String'];
+};
+
+export type Catalog = {
+  __typename?: 'Catalog';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  entryCount: Scalars['Int'];
+};
+
+export type Entry = {
+  __typename?: 'Entry';
+  id: Scalars['ID'];
+  desc: Scalars['String'];
+  imageUrl: Scalars['String'];
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  email: Scalars['String'];
+  catalogs?: Maybe<Array<Catalog>>;
+  entries?: Maybe<Array<Entry>>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  addCatalog?: Maybe<Catalog>;
+  removeCatalog: Scalars['Boolean'];
+  updateCatalog: Catalog;
+  addEntry: Scalars['Boolean'];
+  removeEntry: Entry;
+  updateEntry: Entry;
+  changePassword?: Maybe<User>;
+  confirmUser: Scalars['Boolean'];
+  forgotPassword: Scalars['Boolean'];
+  login?: Maybe<User>;
+  logout: Scalars['Boolean'];
+  register: RegisterResponse;
+};
+
+
+export type MutationAddCatalogArgs = {
+  newCatalogData: CreateCatalogInput;
+};
+
+
+export type MutationRemoveCatalogArgs = {
+  id: Scalars['String'];
+};
+
+
+export type MutationUpdateCatalogArgs = {
+  data: UpdateCatalogInput;
+};
+
+
+export type MutationAddEntryArgs = {
+  catalogId?: Maybe<Scalars['String']>;
+  description: Scalars['String'];
+  photo: Scalars['Upload'];
+};
+
+
+export type MutationRemoveEntryArgs = {
+  catalogId: Scalars['String'];
+  id: Scalars['String'];
+};
+
+
+export type MutationUpdateEntryArgs = {
+  photo?: Maybe<Scalars['Upload']>;
+  data: UpdateEntryInput;
+};
+
+
+export type MutationChangePasswordArgs = {
+  data: ChangePasswordInput;
+};
+
+
+export type MutationConfirmUserArgs = {
+  token: Scalars['String'];
+};
+
+
+export type MutationForgotPasswordArgs = {
+  email: Scalars['String'];
+};
+
+
+export type MutationLoginArgs = {
+  password: Scalars['String'];
+  email: Scalars['String'];
+};
+
+
+export type MutationRegisterArgs = {
+  data: RegisterInput;
+};
+
+export type CreateCatalogInput = {
+  name: Scalars['String'];
+};
+
+export type UpdateCatalogInput = {
+  name: Scalars['String'];
+  id: Scalars['String'];
+};
+
+
+export type UpdateEntryInput = {
+  id: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  currentCatalogId?: Maybe<Scalars['String']>;
+  newCatalogId?: Maybe<Scalars['String']>;
+};
+
+export type ChangePasswordInput = {
+  password: Scalars['String'];
+  token: Scalars['String'];
+};
+
+export type RegisterResponse = {
+  __typename?: 'RegisterResponse';
+  errors?: Maybe<Array<FieldError>>;
+  user?: Maybe<User>;
+};
+
+export type FieldError = {
+  __typename?: 'FieldError';
+  field: Scalars['String'];
+  message: Scalars['String'];
+};
+
+export type RegisterInput = {
+  password: Scalars['String'];
+  email: Scalars['String'];
+};
+
+export type RegisterMutationVariables = Exact<{
+  data: RegisterInput;
+}>;
+
+
+export type RegisterMutation = (
+  { __typename?: 'Mutation' }
+  & { register: (
+    { __typename?: 'RegisterResponse' }
+    & { errors?: Maybe<Array<(
+      { __typename?: 'FieldError' }
+      & Pick<FieldError, 'field' | 'message'>
+    )>>, user?: Maybe<(
+      { __typename?: 'User' }
+      & Pick<User, 'id' | 'email'>
+    )> }
+  ) }
+);
+
+
+export const RegisterDocument = gql`
+    mutation Register($data: RegisterInput!) {
+  register(data: $data) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      email
+    }
+  }
+}
+    `;
+export type RegisterMutationFn = Apollo.MutationFunction<RegisterMutation, RegisterMutationVariables>;
+
+/**
+ * __useRegisterMutation__
+ *
+ * To run a mutation, you first call `useRegisterMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRegisterMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [registerMutation, { data, loading, error }] = useRegisterMutation({
+ *   variables: {
+ *      data: // value for 'data'
+ *   },
+ * });
+ */
+export function useRegisterMutation(baseOptions?: Apollo.MutationHookOptions<RegisterMutation, RegisterMutationVariables>) {
+        return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, baseOptions);
+      }
+export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>;
+export type RegisterMutationResult = Apollo.MutationResult<RegisterMutation>;
+export type RegisterMutationOptions = Apollo.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
