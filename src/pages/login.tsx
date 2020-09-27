654979b109578857d6833d746a4ba65568074012
wojtek <wojciech.bilicki@gmail.com>
2020-09-27 18:44:46 +0200

    Resolve #5. Verify register token page

---
+++ src/pages/login.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Wrapper } from '../components/common/Wrapper';
+
+interface LoginProps {
+
+}
+
+const Login: React.FC<LoginProps> = ({}) => {
+    return (
+      <Wrapper>
+        Dude
+      </Wrapper>
+    );
+}
+
+export default Login; 
