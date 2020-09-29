4a769b72408b1ad54b40c69fca4193091946724d
wojtek <wojciech.bilicki@gmail.com>
2020-09-29 21:32:57 +0200

    Resolve #10. Przelaczanie trybów w aplikacji

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
-const Index = () => (
- <div>Hello world</div>
-)
+import { Button, Flex, Heading } from "@chakra-ui/core"
+import Link from "next/link"
+
+const Index = () => {
+  return (
+    <Flex 
+    backgroundColor="teal.500"
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      color="white">
+        <Flex align="center" mr={5}>
+            <Heading size="lg">
+              Picby
+            </Heading>
+
+        </Flex>
+ 
+        <Button backgroundColor="blue.400">
+          <Link href="/parentMode">
+            Parent Mode
+          </Link>
+        </Button>
+
+    </Flex>
+  )
+  }
 
 export default Index
---
+++ src/parentMode/Layout.tsx
@@ -0,0 +1,55 @@
+import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Flex, Heading, useDisclosure } from "@chakra-ui/core"
+import Link from "next/link"
+import { Wrapper } from "../components/common/Wrapper"
+import MenuButton from "./MenuButton"
+
+const Layout: React.FC = ({children}) => {
+  const {isOpen, onClose, onOpen} = useDisclosure()
+  return (
+    <>
+    <Flex 
+    backgroundColor="teal.500"
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      color="white">
+        <Flex align="center" mr={5}>
+            <MenuButton handleToggle={onOpen}/>
+
+        </Flex>
+ 
+        <Button backgroundColor="blue.400">
+          <Link href="/">
+            ChildMode
+          </Link>
+        </Button>
+
+    </Flex>
+    <Wrapper>
+      {children}
+    <Drawer 
+      isOpen={isOpen}
+      placement="left"
+      onClose={onClose}
+      >
+        <DrawerOverlay />
+    <DrawerContent>
+      <DrawerCloseButton />
+      <DrawerHeader>Menu</DrawerHeader>
+      <DrawerBody>
+          Folder
+      </DrawerBody>
+      <DrawerFooter>
+        <Button>Logout</Button>
+      </DrawerFooter>
+    </DrawerContent>
+    </Drawer>
+    </Wrapper>
+    </>
+  )
+  }
+
+export default Layout 
+
